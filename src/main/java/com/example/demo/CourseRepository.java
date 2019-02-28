f18a78b23740ec32d4631a1afde291e44d12096a
eyael <eyaelja1@gmail.com>
2019-02-27 23:55:58 -0500

    first initial

--- src/main/java/com/example/demo/CourseRepository.java
+++ src/main/java/com/example/demo/CourseRepository.java
@@ -3,10 +3,10 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public class CourseRepository {
+
     public interface CourseRepository extends CrudRepository<Course,
             Long> {
     }
 
-}
+
 
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,14 +25,19 @@ public class HomeController {
         return "courseform";
     }
     @PostMapping("/process")
-    public String processForm(@Valid Course course, BindingResult result){
-        if (result.hasErrors()){
+    public String processForm(@Valid Course course,
+                              BindingResult result) {
+        if (result.hasErrors()) {
             return "courseform";
         }
-        jobRepository.save(job);
-        return"redirect:/";
+        courseRepository.save(course);
+        return "redirect:/";
     }
 
-}
+     @RequestMapping("/detail/{id}")
+             public String showCourse(@PathVariable("id") long id, Model model)
+     {
+         model.addAttribute("course", CourseRepository.findOne(id))
+     }
 
 }
